d9a0ce8e6f981624b216e4ec70f5affa6d6ace6a
MetalG <fra_borjon@hotmail.com>
2023-03-24 20:43:29 -0600

    add: comentarios

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,2 +1,4 @@
+// Asigna el valor de nombre que se capture en el prompt
 const nombre = prompt('Cual es tu nombre?');
+// Toma el nombre del prompt y lo muestra en pantalla con un inner
 document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JS moderno`;
