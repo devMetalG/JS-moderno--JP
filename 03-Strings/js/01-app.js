a5a91052e522768cc87b46e0f9331d11fc6700f9
MetalG <fra_borjon@hotmail.com>
2023-03-25 01:28:41 -0600

    add: create strings

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,11 @@
+// Crear String 1ra forma
+const producto = "Monitor1";
+console.log(producto);
+
+// Crear String 2da forma
+const producto2 = String('Monitor2');
+console.log(producto2);
+
+// Crear String 3ra forma
+const producto3 = new String('Monitor3');
+console.log(producto3);
