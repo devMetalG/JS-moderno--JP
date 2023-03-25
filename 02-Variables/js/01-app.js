d8caa247affe6d1bbb2698bfdc5fd508e4070bbe
MetalG <fra_borjon@hotmail.com>
2023-03-24 20:54:05 -0600

    add: create variables with var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,23 @@
+// Creo variable
+var producto = "Monitor 24 pulgadas";
+console.log(producto);
+
+// Reasigno la variable string
+producto = "Monitor 27 pulgadas";
+console.log(producto);
+
+// Reasigno la variable con numero
+producto = 24;
+console.log(producto);
+
+// Se crea variable sin valor
+var disponible;
+disponible = false;
+
+// Inicializo multiples variables con coma ','
+var var1,
+    var2,
+    var3;
+
+// Crear variables con camelCase
+var nombreUsuario;
