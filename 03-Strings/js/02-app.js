4ad233d42e07b4295dac0d83c8ca2d1aa98f3dc5
MetalG <fra_borjon@hotmail.com>
2023-03-25 12:14:33 -0600

    add: string methods

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,12 @@
+const producto = "Monitor1";
+
+console.log(producto);
+
+// Conocer la cantidad de letras 
+console.log(producto.length);
+
+// Indica en que posición del string se encuentra la palabra
+console.log(producto.indexOf('Monitor'));
+
+// Indica si incluye la palabra en el string
+console.log(producto.includes('Monitor'));
