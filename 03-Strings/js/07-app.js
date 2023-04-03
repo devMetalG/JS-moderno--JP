15547cc3850f54c0f5c8bba2bf17de23178b26c5
MetalG <fra_borjon@hotmail.com>
2023-04-02 20:39:39 -0600

    add: toUpperCase, toLowerCase & toString

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,11 @@
+const producto = 'Monitor 20 pulgadas'; 
+
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+const email = 'CORREO@correo.com';
+console.log(email.toLowerCase());
+
+const precio = 200;
+console.log(precio);
+console.log(precio.toString());
