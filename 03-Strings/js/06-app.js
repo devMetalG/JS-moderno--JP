989396c30fd7f3cb74ab7d196d5ed964fdf0c71a
MetalG <fra_borjon@hotmail.com>
2023-04-02 20:17:48 -0600

    add: Repeat & split

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,16 @@
+const producto = 'Monitor1 20 pulgadas';
+
+// repeat 
+// repite un texto
+const texto = 'en promo!'.repeat(3);
+console.log(texto);
+
+console.log(`${producto} ${texto}`);
+
+// split
+// divide un string
+const actividad = 'Estoy aprendiendo JavaScript';
+console.log(actividad.split(" "));
+
+const hobbies = 'programar, dibujar, tocar guitarra, ver anime'
+console.log(hobbies.split(", "));
