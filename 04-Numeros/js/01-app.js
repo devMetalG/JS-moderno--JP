1429362ce95db1375dba69517f8c1e5997087a5b
MetalG <fra_borjon@hotmail.com>
2023-04-06 00:28:35 -0600

    crear numeros

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+// crear numeros
+
+const n1 = 20;
+const n2 = 30;
+const n3 = 30.1;
+const n4 = -30.1;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+
+const n5 = new Number(80);
+
+console.log(n5);
