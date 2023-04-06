f60887263c43df6b8d586bd75c34fb04ec714b28
MetalG <fra_borjon@hotmail.com>
2023-04-06 01:03:19 -0600

    math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,43 @@
+// Math
+let res;
+
+// PI
+res = Math.PI;
+console.log(res);
+
+// round: redondear
+res = Math.round(2.1);
+console.log(res);
+
+// ceil: redondear solo hacia arriba 
+res = Math.ceil(3.1);
+console.log(res);
+
+// floor: redondear solo hacia abajo
+res = Math.floor(3.6);
+console.log(res);
+
+// sqrt: raíz cuadrada
+res = Math.sqrt(25);
+console.log(res);
+
+// abs: valor absoluto
+res = Math.abs(-45);
+console.log(res);
+
+// pow: potenciar un numero
+res = Math.pow(2,4);
+console.log(res);
+
+// min: devuelve el numero mínimo
+res = Math.min(1,5);
+console.log(res);
+
+// max: devuelve el numero máximo 
+res = Math.max(10,12);
+console.log(res);
+
+// random
+res = Math.random();
+console.log(res);
+
