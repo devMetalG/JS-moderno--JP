0525c40fe2d81b25dad1d1f353d9b35fde909c1d
MetalG <fra_borjon@hotmail.com>
2023-04-06 00:34:53 -0600

    Operaciones

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,22 @@
+const n1 = 30;
+const n2 = 20;
+
+let res;
+
+// Suma 
+res = n1 + n2;
+
+// Resta
+res = n1 - n2;
+
+// Multiplicación
+res = n1 * n2;
+
+//división
+res = n1 / n2;
+
+// Modulo
+res = n1 % n2;
+
+
+console.log(res);
