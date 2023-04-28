1c8b0ed83c84b540a964fac655820253b71f5ac4
MetalG <fra_borjon@hotmail.com>
2023-04-27 23:54:01 -0600

    add: Incrementos & Decrementos

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,35 @@
+
+// incremento
+let puntajePlus = 5;
+let plusPuntaje = 5;
+
+// decremento
+let puntajeMinus = 5;
+let minusPuntaje = 5;
+
+// primero trae el puntaje y después lo suma
+console.log('___________________________________');
+console.log('############ Puntaje++ ############');
+console.log(`Puntaje original: ${puntajePlus}`);
+console.log(`Primero muestra la variable: ${puntajePlus++}`); 
+console.log(`Después la suma: ${puntajePlus}`); 
+console.log('___________________________________');
+// hace la suma y lo muestra en el momento 
+console.log('############ ++Puntaje ############');
+console.log(`Puntaje original: ${plusPuntaje}`);
+console.log(`Realiza la suma primero: ${++plusPuntaje}`); 
+console.log(`Después de la suma: ${plusPuntaje}`);
+console.log('___________________________________');
+// primero trae el puntaje y después lo resta
+console.log('############ Puntaje-- ############');
+console.log(`Puntaje original: ${puntajeMinus}`);
+console.log(`Primero muestra la variable: ${puntajePlus--}`); 
+console.log(`Después la resta: ${puntajePlus}`); 
+console.log('___________________________________');
+// hace la resta y lo muestra en el momento 
+console.log('############ --Puntaje ############');
+console.log(`Puntaje original: ${minusPuntaje}`);
+console.log(`Realiza la resta primero: ${++plusPuntaje}`); 
+console.log(`Después de la resta: ${minusPuntaje}`);
+console.log('___________________________________');
+
