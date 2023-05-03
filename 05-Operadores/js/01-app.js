72554599d9758b1a08f512ac7fdca05bf6e1f3fa
MetalG <fra_borjon@hotmail.com>
2023-05-02 19:50:46 -0600

    add: operadores > & <

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,13 @@
+const n1 = 20;
+const n2 = "20";
+const n3 = 30;
+
+// Mayor que >
+
+console.log( n1 > n3 );
+console.log( n3 > n1 );
+
+// Menor que <
+
+console.log (n1 < n3 );
+console.log( n3 < n1 );
