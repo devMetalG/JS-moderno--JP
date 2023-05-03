de6da779c7767b034826f7e255edd36f57732aba
MetalG <fra_borjon@hotmail.com>
2023-05-02 19:58:26 -0600

    add: operadores ==, ===, != & !==

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,23 @@
+const n1 = 20;
+const n2 = "20";
+const n3 = 30;
+
+// Revisar si 2 numeros son iguales
+
+console.log(n1 == n3);
+console.log(n1 == n2);
+
+// Comparador estricto 
+
+console.log(n1 === n2);
+console.log(n1 === parseInt(n2));
+
+// Diferentes
+
+const pas1 = "admin";
+const pas2 = "Admin";
+
+console.log(pas1 != pas2);
+console.log(n1 != n2);
+console.log(n1 !== n2);
+console.log(n1 !== parseInt(n2));
