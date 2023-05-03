86b9c0a94d6da608fe2ba62c207ed5e1ef134ecf
MetalG <fra_borjon@hotmail.com>
2023-05-02 20:03:26 -0600

    add: comparar Undefined & Null

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,16 @@
+// Undefined
+
+let n1;
+
+console.log(n1);
+console.log(typeof(n1));
+
+// Null
+
+let n2 = null;
+
+console.log(n2);
+console.log(typeof(n2));
+
+console.log(n1 == n2);
+console.log(n1 === n2);
