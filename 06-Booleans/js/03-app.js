0ac63d80d8562369517cbb9d4c00924516cd5bc4
MetalG <fra_borjon@hotmail.com>
2023-08-14 21:06:53 -0600

    add: operador ternario

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const autenticado = true; 
+
+if (autenticado) {
+  console.log("Puedes ver anime");
+} else {
+  console.log("No estás autenticado");
+}
+
+const autenticado2 = true;
+console.log(autenticado2 ? "Puedes ver animu" : "No tienes cuenta");
