55606f2a0c5d8d3fdfeab13782fd82655c18189f
MetalG <fra_borjon@hotmail.com>
2023-08-19 16:45:43 -0600

    add: crear objetos

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,15 @@
+const nombre = 'Monitor';
+const precio = 3000;
+const disponible = true;
+
+// Un objeto es una colección de propiedades, agrupa variables 
+
+// Object Literal 
+
+const producto = {
+  nombre: 'Monitor',
+  precio: 300,
+  disponible: true
+}
+
+console.log(producto);
