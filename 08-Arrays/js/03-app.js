ad2cd56290bf25f5f0e26079de59f9e6199a85a0
MetalG <fra_borjon@hotmail.com>
2023-09-20 16:34:33 -0600

    add: recorrer un arreglo

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,12 @@
+const meses= ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+console.log(meses);
+console.table(meses);
+
+// cuanto mide el arreglo 
+
+console.log(meses.length);
+
+for(let i = 0; i < meses.length; i++){
+  console.log(meses[i]);
+}
