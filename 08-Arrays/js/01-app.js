b99e6c1f005414e430cd78db0cc0d1405db7b432
MetalG <fra_borjon@hotmail.com>
2023-09-20 16:10:10 -0600

    add: crear arrays

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,11 @@
+const numeros = [10, 20, 30];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+
+console.log(numeros);
+console.log(meses);
+
+// Arreglo con datos de todo tipo
+
+const deTodo = ["hola", 10, true, 'si', null, {nombre: 'Juan', trabajo: 'programador'}, [1,2,3]];
+console.log(deTodo);
