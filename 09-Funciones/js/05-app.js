5ef97cb693a212aeeede2eeb39482f0c55f38f40
MetalG <fra_borjon@hotmail.com>
2023-09-23 17:03:28 -0600

    add: parametros y argumentos en funciones

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,14 @@
+function sumar(a, b){ // a y b son parametros de función
+  console.log(a + b);
+};
+
+sumar(2, 3); // 2 y 3 son argumentos de función
+sumar(200, 321); 
+
+function saludar(nombre, apellido) {
+  console.log(`Hola, ${nombre} ${apellido}`)
+};
+
+saludar('Memo', 'Franco');
+saludar('Memo');
+saludar();
