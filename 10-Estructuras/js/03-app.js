8a4ecdd738c7cb80276db2f755498a6ff8474e67
MetalG <fra_borjon@hotmail.com>
2023-09-26 00:47:56 -0600

    add: mayor que y menor que if

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,10 @@
+// Operador mayor que y menor que
+
+const dinero = 500;
+const totalPagar = 300;
+
+if (dinero >= totalPagar) {
+  console.log('Si podemos pagar');
+} else {
+  console.log('No tienes fondos suficientes');
+}
