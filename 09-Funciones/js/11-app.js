837fc06ec17cce12ca27963580cfb558bed67827
MetalG <fra_borjon@hotmail.com>
2023-09-25 18:24:04 -0600

    add: ventajas de arrow functions

---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,9 @@
+const aprendiendo = function(lenguaje, lenguaje2){
+  console.log(`Aprendiendo ${lenguaje} y ${lenguaje2}`);
+};
+
+aprendiendo('JavaScript', 'Node.js');
+
+const aprendiendo2 = (tecno, tecno2) => `Aprendiendo ${tecno} y ${tecno2}`;
+
+console.log(aprendiendo2('JS', 'Node.js'));
