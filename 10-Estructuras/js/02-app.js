14e92c389db55a7ac05b8ce0916e34b37caea7b3
MetalG <fra_borjon@hotmail.com>
2023-09-26 00:42:33 -0600

    add: comparador estricto if

---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,19 @@
+const puntaje = 1000;
+
+if(puntaje != 1000){
+  console.log('si es diferente...');
+} else {
+  console.log('No es diferente...');
+}
+
+if(puntaje == "1000"){
+  console.log('si es igual...');
+} else {
+  console.log('No es igual...');
+}
+
+if(puntaje === "1000"){
+  console.log('si es igual...');
+} else {
+  console.log('No es igual...');
+}
