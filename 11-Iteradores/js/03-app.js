1dadfc4cddd38dacc3f1aedb02a855946ca536c6
MetalG <fra_borjon@hotmail.com>
2023-09-26 20:35:34 -0600

    add: while loop

--- 11-Iteradores/js/03-app.js
+++ 11-Iteradores/js/03-app.js
@@ -2,14 +2,11 @@
 
 for (let i = 1; i <= 100; i++) {
   if (i%3 == 0 && i%5 == 0) {
-    console.log('Fizz Buzz');
-    continue;
+    console.log(`${i} Fizz Buzz`);
   } else if (i%3 == 0) {
-    console.log('Fizz');
-    continue;
+    console.log(`${i} Fizz`);
   } else if (i%5 == 0) {
-    console.log('Buzz');
-    continue;
+    console.log(`${i} Buzz`);
   } else {
     console.log(i);
   }
---
+++ 11-Iteradores/js/04-app.js
@@ -0,0 +1,10 @@
+let i = 1;
+while (i <= 10) {
+  if (i%2 == 0) {
+    console.log(`NUmero: ${i} es Par`)
+    
+  } else {
+    console.log(`NUmero: ${i} es Impar`);
+  }
+  i++;
+}
