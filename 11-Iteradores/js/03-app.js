01c3e789755160c181c94e2a9d4b701d4e1d855b
MetalG <fra_borjon@hotmail.com>
2023-09-26 20:12:18 -0600

    add: fizz buzz

---
+++ 11-Iteradores/js/03-app.js
@@ -0,0 +1,16 @@
+// Fizz buzz
+
+for (let i = 1; i <= 100; i++) {
+  if (i%3 == 0 && i%5 == 0) {
+    console.log('Fizz Buzz');
+    continue;
+  } else if (i%3 == 0) {
+    console.log('Fizz');
+    continue;
+  } else if (i%5 == 0) {
+    console.log('Buzz');
+    continue;
+  } else {
+    console.log(i);
+  }
+}
