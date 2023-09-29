12db07d6b945a25bf42df8476a0edb9d87bbbb42
MetalG <fra_borjon@hotmail.com>
2023-09-29 14:59:17 -0600

    add: querySelector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,17 @@
+const card = document.querySelector('.card');
+console.log(card);
+
+const info = document.querySelector('.premium .info');
+console.log(info); 
+
+// Seleccionar 2do card de hospedaje 
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+// Seleccionar formulario id
+const formulario = document.querySelector('#formulario');
+console.log(formulario);
+
+// Seleccionar elementos HTML
+const navegacion = document.querySelector('nav');
+console.log(navegacion);
