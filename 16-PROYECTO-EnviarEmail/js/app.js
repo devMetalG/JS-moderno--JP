b263051008f6d84e31635e51484c8f05f9e862eb
MetalG <fra_borjon@hotmail.com>
2023-10-03 17:19:57 -0600

    add: reset de formulario, proyecto-emails

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -12,11 +12,23 @@ document.addEventListener('DOMContentLoaded', function (){
   const inputMensaje = document.querySelector('#mensaje');
   const formulario = document.querySelector('#formulario');
   const btnSumbit = document.querySelector('#formulario button[type="submit"]');
+  const btnReset = document.querySelector('#formulario button[type="reset"]');
 
   // Asignar eventos 
   inputEmail.addEventListener('input', validar);
   inputAsunto.addEventListener('input', validar);
   inputMensaje.addEventListener('input', validar);
+  btnReset.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // reiniciar el objeto
+    email.email = '';
+    email.asunto = '';
+    email.mensaje = '';
+
+    formulario.reset();
+    comprobarEmail();
+  })
 
   function validar(e) {
     if (e.target.value.trim() === '') {
