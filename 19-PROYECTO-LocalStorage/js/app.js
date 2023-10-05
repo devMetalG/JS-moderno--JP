80a52c6b3913d9b3bdd14362970d98a3b51b1a93
MetalG <fra_borjon@hotmail.com>
2023-10-04 23:57:50 -0600

    add: validando el formulario, proyecto-tweetsLS

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -12,5 +12,29 @@ function eventListenerts() {
 // Funciones
 function agregarTweet(e) {
   e.preventDefault();
+  // TextArea donde el usario escribe
+  const tweet = document.querySelector('#tweet').value;
+  // Validación
+  if (tweet === '') {
+    mostrarError('Un mensaje no puede ir vacio');
+
+    return;
+  }
   console.log('Agregando tweet');
+}
+
+// Mostrar mensaje de error
+function mostrarError(error){
+  const mensajeError = document.createElement('P');
+  mensajeError.textContent = error;
+  mensajeError.classList.add('error');
+
+  // Insertar en contenido 
+  const contenido = document.querySelector('#contenido');
+  contenido.appendChild(mensajeError);
+
+  // Elimina alerta después de 3s
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 3000);
 }
