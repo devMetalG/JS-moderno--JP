7cd5e9551b3a74aedf9933286e3c2f720ce405b1
MetalG <fra_borjon@hotmail.com>
2023-10-06 18:24:52 -0600

    add: primeros pasos, proyecto-cotizadorPrototypes

---
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -0,0 +1,33 @@
+// Constructores 
+
+function Seguro(marca, year, tipo){
+  this.marca = marca;
+  this.year = year;
+  this.tipo = tipo;
+}
+
+function UI(){}
+
+// Llena las opciones de los años
+UI.prototype.llenarOpciones = () =>{
+  const max = new Date().getFullYear();
+  const min = max - 20;
+
+  const selectYear = document.querySelector('#year');
+
+  for(let i = max; i >=min;i--){
+    let option = document.createElement('OPTION');
+    option.value = i;
+    option.textContent = i;
+    selectYear.appendChild(option);
+  }
+}
+
+// Instanciar UI
+
+const ui = new UI();
+console.log(ui);
+
+document,addEventListener('DOMContentLoaded', ()=>{
+  ui.llenarOpciones(); // Llena select con los años
+});
