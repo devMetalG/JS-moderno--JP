697e6b5e9e9c2fdf4a74345ab6350c2baf709f36
MetalG <fra_borjon@hotmail.com>
2023-10-08 13:38:24 -0600

    add: calculo del seguro, proyecto-cotizadorPrototypes

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -6,6 +6,52 @@ function Seguro(marca, year, tipo){
   this.tipo = tipo;
 }
 
+// Realiza la cotización con los datos
+Seguro.prototype.cotizarSeguro = function(){
+  /* 
+    1 = Americano 1.15
+    2 = Asiatico 1.05
+    3 = Europeo 1.35 
+  */
+
+  let cantidad;
+  const base = 2000
+  console.log(this.marca);
+  switch (this.marca) {
+    case '1':
+      cantidad = base * 1.15;
+      break;
+    case '2':
+      cantidad = base * 1.05;
+      break;
+    case '3':
+      cantidad = base * 1.35;
+      break;
+  
+    default:
+      break;
+  }
+
+  // Leer año
+  const diferencia = new Date().getFullYear() - this.year;
+
+  // cada año más viejo el costo baja un 3%
+  cantidad -= ((diferencia *0.03) * cantidad);
+
+  /*
+    Si el seguro es básico se múltiplica por 30% más
+    Si el seguro es completo se múltiplica por 50% más
+  */
+
+    if (tipo === 'basico') {
+      cantidad *= 1.30;
+    } else {
+      cantidad *= 1.50;
+    }
+
+    return cantidad;
+}
+
 function UI(){}
 
 // Llena las opciones de los años
@@ -78,4 +124,10 @@ function cotizarSeguro(e){
     return;
   } 
   ui.mostrarMensaje('Cotizando...', 'correcto');
+
+  // Instanciar el seguro
+const seguro = new Seguro(marca, year, tipo);
+seguro.cotizarSeguro();
+
+  // Utilizar el prototyoe que va a cotizar
 }
