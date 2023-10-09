90cb85cb6ca714d5c7fa79123446df9d1253b3cd
MetalG <fra_borjon@hotmail.com>
2023-10-09 17:20:45 -0600

    add: metodos y static en classes

--- 24-POO/js/01-app.js
+++ 24-POO/js/01-app.js
@@ -3,17 +3,32 @@ class Cliente {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+
+  mostrarInformacion(){
+    return `Cliente: ${this.nombre} con saldo de: $${this.saldo}`;
+  }
+
+  static bienvenida(){
+    return `Bienvenido al cajero`;
+  }
+
 }
 
 const memo = new Cliente('memo', 1000);
+console.log(memo.mostrarInformacion());
 console.log(memo);
+console.log(Cliente.bienvenida());
 
 const Cliente2 = class {
   constructor(nombre, saldo){
     this.nombre = nombre;
     this.saldo = saldo;
   }
+  mostrarInformacion(){
+    return `Cliente: ${this.nombre} con saldo de: $${this.saldo}`;
+  }
 }
 
 const costal = new Cliente2('Josias', 2000);
+console.log(costal.mostrarInformacion());
 console.log(costal);
