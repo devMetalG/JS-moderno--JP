e0c020b14e52e7e8ac7105e7457aaa9087f1770c
MetalG <fra_borjon@hotmail.com>
2023-10-10 14:56:46 -0600

    add: validar formulario, proyecto-presupuestoClasses

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -5,6 +5,7 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners(){
   document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+  formulario.addEventListener('submit', agregarGasto);
 }
 // Classes
 class Presupuesto {
@@ -20,6 +21,24 @@ class UI {
     const {presupuesto, restante} = cantidad;
     document.querySelector('#total').textContent = presupuesto;
     document.querySelector('#restante').textContent = restante;
+  }
+  imprimirAlerta(mensaje, tipo){
+    const divMensaje = document.createElement('DIV');
+    divMensaje.classList.add('text-center', 'alert');
+
+    if (tipo === 'error') {
+      divMensaje.classList.add('alert-danger');
+    } else {
+      divMensaje.classList.add('alert-success');
+    }
+    // Mensaje error
+    divMensaje.textContent = mensaje;
+    document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+    // Quitar del HTML
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
   }
 }
 
@@ -39,4 +58,20 @@ function preguntarPresupuesto(){
   presupuesto = new Presupuesto(presupuestoUsuario);
   console.log(presupuesto);
   ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e){
+  e.preventDefault();
+
+  const nombre = document.querySelector('#gasto').value;
+  const cantidad = document.querySelector('#cantidad').value;
+
+  if (nombre === '' || cantidad === '') {
+    ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+    return;
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    ui.imprimirAlerta('Cantidad no valida', 'error');
+    return;
+  }
+  console.log('Agregando un gasto');
 }
