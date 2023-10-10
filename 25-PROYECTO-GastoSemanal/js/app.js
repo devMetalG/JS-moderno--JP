360c78a7a1aa2affefd4baa769267d7e5b3f048f
MetalG <fra_borjon@hotmail.com>
2023-10-10 14:13:36 -0600

    add: clase presupuesto, proyecto-presupuestoClasses

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -7,6 +7,21 @@ function eventListeners(){
   document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 // Classes
+class Presupuesto {
+ constructor(presupuesto){
+  this.presupuesto = Number(presupuesto);
+  this.restante = Number(presupuesto);
+  this.gastos = [];
+ }
+}
+
+class UI {
+
+}
+
+// Instanciar 
+const ui = new UI();
+let presupuesto;
 
 // Funciones
 function preguntarPresupuesto(){
@@ -16,4 +31,7 @@ function preguntarPresupuesto(){
   || presupuestoUsuario <= 0) {
     window.location.reload();
   }
+  // Presupuesto valido
+  presupuesto = new Presupuesto(presupuestoUsuario);
+  console.log(presupuesto);
 }
