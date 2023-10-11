6d63a6f4d1dfea00c3f8cb9f5f2bb5abf4b5b010
MetalG <fra_borjon@hotmail.com>
2023-10-11 14:17:27 -0600

    add: mensaje de error y validacion, proyecto-citas

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -17,6 +17,40 @@ const citaObj = {
   sintomas: ''
 }
 
+// Clases
+class Citas{
+  constructor(){
+    this.citas = [];
+  }
+}
+
+class UI {
+  imprimirAlerta(mensaje, tipo) {
+    
+    const divMensaje = document.createElement('DIV');
+    
+    divMensaje.classList.add('text-center', 'alert', 'display-block', 'col-12');
+
+    if (tipo === 'error') {
+      divMensaje.classList.add('alert-danger');
+    } else {
+      divMensaje.classList.add('alert-success');
+    }
+    // Mensaje
+    divMensaje.textContent = mensaje;
+
+    // agregar al HTML
+    document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+    // Quitar alerta
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3500);
+  }
+}
+
+const ui = new UI();
+const administrarCitas = new Citas();
 // event Listeners
 eventListeners();
 function eventListeners() {
@@ -26,10 +60,25 @@ function eventListeners() {
   fechaInput.addEventListener('input', datosCita);
   horaInput.addEventListener('input', datosCita);
   sintomasInput.addEventListener('input', datosCita);
+  formulario.addEventListener('submit', nuevaCita)
 }
 
 // funciones
 function datosCita(e){
   citaObj[e.target.name] = e.target.value;
-  console.log(citaObj);
+}
+
+function nuevaCita(e){
+  e.preventDefault();
+
+  const {mascota, propietario, telefono, fecha, hora, sintomas} = citaObj;
+
+  // validar
+  if (mascota === '' || propietario === '' || telefono === ''
+   || fecha === '' || hora === '' || sintomas === '' ) {
+    ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+    return;
+  }
+
+
 }
