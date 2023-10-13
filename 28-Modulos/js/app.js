b59766c9fcbc3430ca5d039c07c6bf00fbadb838
MetalG <fra_borjon@hotmail.com>
2023-10-12 21:42:08 -0600

    add: heredar una clase importada

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,6 @@
 import { nombreCliente, ahorro, mostrarInfo, tieneSaldo, Cliente } from "./cliente.js";
+import { Empresa } from "./empresa.js";
+
 console.log(nombreCliente);
 console.log(ahorro);
 
@@ -9,3 +11,7 @@ tieneSaldo(ahorro);
 const cliente = new Cliente(nombreCliente, ahorro);
 console.log(cliente);
 console.log(cliente.mostrarInfoCliente());
+
+const empresa = new Empresa('Maguito', 20000, 'Cursos online');
+console.log(empresa);
+console.log(empresa.mostrarInfoEmpresa());
