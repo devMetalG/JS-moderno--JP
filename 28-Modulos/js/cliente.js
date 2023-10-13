dd4aa1d3c7b65928fa31d228cdcbb10c040cdeed
MetalG <fra_borjon@hotmail.com>
2023-10-12 21:11:53 -0600

    add: exportar e importar funciones

--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -5,3 +5,15 @@
 
 export const nombreCliente = "Memo";
 export const ahorro = 200;
+
+export function mostrarInfo (nombre, ahorro) {
+  return `Cliente ${nombre} - Ahorro ${ahorro}`;
+}
+
+export function tieneSaldo(ahorro) {
+  if (ahorro > 0) {
+    console.log('Tiene saldo');
+  } else {
+    console.log('No tiene saldo');
+  }
+}
