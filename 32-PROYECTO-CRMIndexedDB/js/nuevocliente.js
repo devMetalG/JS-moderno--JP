230a29f7f1b4134d860784a8854373b562e95af9
MetalG <fra_borjon@hotmail.com>
2023-10-19 17:36:03 -0600

    add: agregar clientes, proyecto-CRM

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -25,13 +25,42 @@
 
     // Leer inputs
     const nombre = document.querySelector('#nombre').value;
-    const correo = document.querySelector('#email').value;
+    const email = document.querySelector('#email').value;
     const telefono = document.querySelector('#telefono').value;
     const empresa = document.querySelector('#empresa').value;
 
     if (nombre === '' || email === '' || telefono === '' || empresa === '') {
       imprimirAlerta('Todos los campos son obligatorios','error');
       return;
+    }
+
+    // Crear un objeto Cliente
+    const cliente = {
+      nombre,
+      email, 
+      telefono,
+      empresa, 
+    }
+    cliente.id = Date.now();
+    crearCliente(cliente);
+  }
+
+  function crearCliente(cliente){
+    const transaction = DB.transaction(['crm'], 'readwrite');
+    
+    const objectStore = transaction.objectStore('crm');
+    objectStore.add(cliente);
+
+    transaction.onerror = function() {
+      imprimirAlerta('Hubo un error.', 'error');
+    }
+
+    transaction.oncomplete = function() {
+      imprimirAlerta('El cliente se agrego correctamente');
+
+      setTimeout(() => {
+        window.location.href = 'index.html';
+      }, 3000);
     }
   }
 
