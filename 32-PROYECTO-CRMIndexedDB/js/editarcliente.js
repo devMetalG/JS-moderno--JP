657454865d99407221660a2d1784ce64938c6a17
MetalG <fra_borjon@hotmail.com>
2023-10-19 20:44:50 -0600

    add: editar cliente, proyecto-CRM

--- 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
@@ -1,5 +1,6 @@
 (function(){
-  let DB;
+  let idCliente;
+
   const nombreInput = document.querySelector('#nombre');
   const emailInput = document.querySelector('#email');
   const telefonoInput = document.querySelector('#telefono');
@@ -8,15 +9,53 @@
   document.addEventListener('DOMContentLoaded', () => {
     conectarDB();
 
+    // Actualiza el registro 
+    formulario.addEventListener('submit', actualizarCliente)
     // Verificar el ID de la URL
     const parametrosURL = new URLSearchParams(window.location.search);
-    const idCliente = parametrosURL.get('id');
+    idCliente = parametrosURL.get('id');
     if (idCliente) {
       setTimeout(() => {
         obtenerCliente(idCliente);
       }, 100);
     }
   });
+
+  function actualizarCliente(e){
+    e.preventDefault();
+
+    if (nombreInput.value === '' || emailInput.value === '' ||
+        telefonoInput.value === '' || empresaInput.value === '') {
+      imprimirAlerta('Todos los campos son obligatorios', 'error');
+      return;
+    }
+
+    // Actualizar cliente
+    const clienteActualizado = {
+      nombre: nombreInput.value,
+      email: emailInput.value,
+      telefono: telefonoInput.value,
+      empresa: empresaInput.value,
+      id: Number(idCliente)
+    }
+
+    const transaction = DB.transaction(['crm'], 'readwrite');
+    const objectStore = transaction.objectStore('crm');
+
+    objectStore.put(clienteActualizado);
+
+    transaction.oncomplete = function(){
+      imprimirAlerta('Editado correctamente');
+
+      setTimeout(() => {
+        window.location.href = 'index.html';
+      }, 3000);
+    }
+    
+    transaction.onerror = function(){
+      imprimirAlerta('Hubo un error', 'error');
+    }
+  }
 
   function obtenerCliente(id){
     const transaction = DB.transaction(['crm'], 'readonly');
@@ -32,18 +71,6 @@
         }
         cursor.continue();
       }
-    }
-  }
-
-  function conectarDB(){
-    const abrirConexion = window.indexedDB.open('crm', 1);
-
-    abrirConexion.onerror = function(){
-      console.log('Hubo un error.');
-    }
-
-    abrirConexion.onsuccess = function(){
-      DB = abrirConexion.result;
     }
   }
 
