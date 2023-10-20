5360c03203e58dd30aed9afc3e405ec5cff9e7ab
MetalG <fra_borjon@hotmail.com>
2023-10-20 17:19:30 -0600

    add: Detectar conexión a internet

---
+++ 34-APIs-JS/js/03-app.js
@@ -0,0 +1,12 @@
+window.addEventListener('online', actualizarEstado)
+window.addEventListener('offline', actualizarEstado)
+
+function actualizarEstado(){
+  if (navigator.onLine) {
+    console.log('Si estás conectado')
+  }else{
+    console.log('No estás conectado')
+  }
+}
+
+actualizarEstado()
