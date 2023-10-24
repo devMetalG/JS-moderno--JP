0e402a9687afe22c397fc90a31f1e0039cd5705b
Guillermo Franco <fra_borjon@hotmail.com>
2023-10-23 19:54:09 -0600

    add: mostrar las categorias, proyecto-Recetas

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,10 +1,24 @@
 function iniciarApp(){
+
+  const selectCategorias = document.querySelector('#categorias')
+
   obtenerCategorias()
   function obtenerCategorias(){
     const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
     fetch(url)
       .then(response => response.json())
-      .then(result => console.log(result))
+      .then(result => mostrarCategorias(result.categories))
+  }
+
+  function mostrarCategorias(categorias = []){
+    categorias.forEach(categoria => {
+      const {strCategory} = categoria
+      const option = document.createElement('OPTION')
+      option.value = strCategory
+      option.textContent = strCategory
+
+      selectCategorias.appendChild(option)
+    });
   }
 }
 
