025130a0228a72365196760c314bd71746308580
Guillermo Franco <fra_borjon@hotmail.com>
2023-10-23 20:07:21 -0600

    add: seleccionar categoria, proyecto-Recetas

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,6 +1,7 @@
 function iniciarApp(){
 
   const selectCategorias = document.querySelector('#categorias')
+  selectCategorias.addEventListener('change', seleccionarCategoria)
 
   obtenerCategorias()
   function obtenerCategorias(){
@@ -19,6 +20,12 @@ function iniciarApp(){
 
       selectCategorias.appendChild(option)
     });
+  }
+
+  function seleccionarCategoria(e){
+    const categoria = e.target.value
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
+    console.log(url)
   }
 }
 
