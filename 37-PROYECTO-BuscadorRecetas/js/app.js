64fdbf627102ab9a924a4b380780b34e759a472e
Guillermo Franco <fra_borjon@hotmail.com>
2023-10-23 20:12:14 -0600

    add: obtener platillos, proyecto-Recetas

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -25,7 +25,10 @@ function iniciarApp(){
   function seleccionarCategoria(e){
     const categoria = e.target.value
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
-    console.log(url)
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(result => console.log(result))
   }
 }
 
