1de1119cca6656e0af1baba0114d99c94df32e9c
Guillermo Franco <fra_borjon@hotmail.com>
2023-10-25 19:50:31 -0600

    add: mostrar favoritos, proyecto-Recetas

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,12 +1,19 @@
 function iniciarApp(){
 
-  const selectCategorias = document.querySelector('#categorias')
-  selectCategorias.addEventListener('change', seleccionarCategoria)
-
   const resultado = document.querySelector('#resultado')
+  const selectCategorias = document.querySelector('#categorias')
+
+  if (selectCategorias) {
+    selectCategorias.addEventListener('change', seleccionarCategoria)
+    obtenerCategorias()
+  }
+  const favoritosDiv = document.querySelector('.favoritos')
+  if (favoritosDiv) {
+    obtenerFavs()
+  }
+
   const modal = new bootstrap.Modal('#modal', {})
 
-  obtenerCategorias()
   function obtenerCategorias(){
     const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
     fetch(url)
@@ -54,15 +61,15 @@ function iniciarApp(){
 
       const recetaImg = document.createElement('IMG')
       recetaImg.classList.add('card-img-top')
-      recetaImg.alt = `Imagen de la receta ${strMeal}`
-      recetaImg.src = strMealThumb
+      recetaImg.alt = `Imagen de la receta ${strMeal ?? receta.title}`
+      recetaImg.src = strMealThumb ?? receta.img
 
       const recetaCardBody = document.createElement('DIV')
       recetaCardBody.classList.add('card-body')
 
       const recetaHeading = document.createElement('H3')
       recetaHeading. classList.add('card-title', 'mb-3')
-      recetaHeading.textContent = strMeal
+      recetaHeading.textContent = strMeal ?? receta.title
 
       const recetaButton = document.createElement('BUTTON')
       recetaButton.classList.add('btn', 'btn-danger', 'w-100')
@@ -70,7 +77,7 @@ function iniciarApp(){
       // recetaButton.dataset.bsTarget = '#modal'
       // recetaButton.dataset.bsToggle = 'modal'
       recetaButton.onclick = function(){
-        seleccionarReceta(idMeal)
+        seleccionarReceta(idMeal ?? receta.id)
       }
 
       // Inyectar en el HTML
@@ -188,6 +195,18 @@ function iniciarApp(){
     toastBody.textContent = mensaje
 
     toast.show()
+  }
+
+  function obtenerFavs(){
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? []
+    if (favoritos.length) {
+      mostrarRecetas(favoritos)
+      return
+    }
+    const nofavoritos = document.createElement('P')
+    nofavoritos.textContent = 'No hay Favoritos'
+    nofavoritos.classList.add('fs-4', 'text-center', 'font-bold', 'mt-5')
+    favoritosDiv.appendChild(nofavoritos)
   }
 
   function limpiarHTML(selector){
