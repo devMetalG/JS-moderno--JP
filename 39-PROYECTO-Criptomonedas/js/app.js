29b36822d756a4531102b5646353dbbc288db921
Guillermo Franco <fra_borjon@hotmail.com>
2023-10-27 17:50:40 -0600

    add: leer selects, proyecto-cripto

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -1,10 +1,21 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas')
+const monedaSelect = document.querySelector('#moneda')
+const formulario = document.querySelector('#formulario')
+const objBusqueda = {
+  moneda: '',
+  criptomoneda: ''
+}
+
 const obtenerCripo = criptomonedas => new Promise( resolve => {
   resolve(criptomonedas)
 })
 
 document.addEventListener('DOMContentLoaded', () => {
   consultarCripto()
+
+  formulario.addEventListener('submit', enviarFormulario)
+  criptomonedasSelect.addEventListener('change', leerValor)
+  monedaSelect.addEventListener('change', leerValor)
 })
 
 function consultarCripto(){
@@ -24,4 +35,31 @@ function selectCripto(criptomonedas){
     option.textContent = FullName
     criptomonedasSelect.appendChild(option)
   });
+}
+
+function enviarFormulario(e){
+  e.preventDefault()
+  // validar formulario
+  const {moneda, criptomoneda} = objBusqueda
+
+  if (moneda === '' || criptomoneda ==='') {
+    mostrarAlerta('Ambos campos son obligatorios')
+    return
+  }
+}
+
+function leerValor(e){
+  objBusqueda[e.target.name] = e.target.value
+  console.log(objBusqueda)
+}
+
+function mostrarAlerta(mensaje){
+  const resultado = document.querySelector('#resultado')
+  const alerta = document.createElement('DIV')
+  alerta.classList.add('bg-red-100')
+  alerta.textContent = mensaje
+  resultado.appendChild(alerta)
+  setTimeout(() => {
+    alerta.remove()
+  }, 3000);
 }
