f2b4eb54adf988c0129a3bd8cfcf1d00c0783de1
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-01 23:44:50 -0600

    add: Async await declaration

---
+++ 41-AsyncAwait/js/02-app.js
@@ -0,0 +1,27 @@
+function descargarClientes(){
+  return new Promise((resolve, reject) => {
+    const error = true
+
+    setTimeout(() => {
+      if (!error) {
+        resolve('El listado de clientes se descargo correctamente')
+      } else {
+        reject('Error en la conexión')
+      }
+    }, 3000);
+  })
+}
+
+// Async await
+async function ejecutar(){
+  try {
+    console.log(2+1)
+    const respuesta = await descargarClientes()
+    console.log(2+2)
+    console.log(respuesta)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+ejecutar()
