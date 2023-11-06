fc1307aece134886b2b9de978daf1083b925349c
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-06 12:22:09 -0600

    add: hoisting

---
+++ 46-DominandoJavaScript/js/02-app.js
@@ -0,0 +1,11 @@
+obtenerNombre('Memo')
+
+function obtenerNombre(nombre){
+  console.log(`El nombre del cliente es: ${nombre}`)
+}
+
+const obtenerNombre2 = function (nombre){
+  console.log(`El nombre del cliente es: ${nombre}`)
+}
+
+obtenerNombre2('Costal')
