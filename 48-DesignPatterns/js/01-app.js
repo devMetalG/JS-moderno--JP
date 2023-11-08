6db46468daa6cbee1fcec90bf6fad46b5354177e
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-08 15:49:04 -0600

    add: class patern

---
+++ 48-DesignPatterns/js/01-app.js
@@ -0,0 +1,11 @@
+// Class paterm
+
+class Persona {
+  constructor(nombre, email){
+    this.nombre = nombre
+    this.email = email
+  }
+}
+
+const persona = new Persona('Costal', 'costal@hotmail.com')
+console.log(persona)
