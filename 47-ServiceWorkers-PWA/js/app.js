040e6b92db3ba9a0e1632c78d2b4b509d9906ac5
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-07 23:33:36 -0600

    add: cacheando archivos

--- 47-ServiceWorkers-PWA/js/app.js
+++ 47-ServiceWorkers-PWA/js/app.js
@@ -1,5 +1,7 @@
-if ('ServiceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(registrado => console.log('Se instalo SW', registrado))
     .catch(error => console.log('Fallo la instalacion de SW', error))
+}else {
+  console.log('No compatible')
 }
--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -1,4 +1,30 @@
+const nombreCache = 'apv-v1'
+const archivos = [
+  '/47-ServiceWorkers-PWA',
+  '/47-ServiceWorkers-PWA/index.html',
+  '/47-ServiceWorkers-PWA/css/bootstrap.css',
+  '/47-ServiceWorkers-PWA/css/styles.css',
+  '/47-ServiceWorkers-PWA/js/app.js',
+  '/47-ServiceWorkers-PWA/js/apv.js'
+]
 self.addEventListener('install', e => {
   console.log('Instalado')
+
+  e.waitUntil(
+    caches.open(nombreCache)
+      .then( cache => {
+        console.log('Cacheando..')
+        cache.addAll(archivos)
+      })
+  )
+})
+
+self.addEventListener('activate', e => {
+  console.log('SW activado.')
   console.log(e)
+})
+
+// Fetch
+self.addEventListener('fetch', e => {
+  console.log('fetch...',e)
 })
