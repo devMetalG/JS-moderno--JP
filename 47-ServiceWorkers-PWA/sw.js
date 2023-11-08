1116280408d8b18b8a8421406c0c29cb49fe6841
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-08 15:35:10 -0600

    add: error.html

--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -1,6 +1,8 @@
 const nombreCache = 'apv-v1'
 const archivos = [
   '/47-ServiceWorkers-PWA',
+  '/47-ServiceWorkers-PWA/error.html',
+  '/47-ServiceWorkers-PWA/manifest.json',
   '/47-ServiceWorkers-PWA/index.html',
   '/47-ServiceWorkers-PWA/css/bootstrap.css',
   '/47-ServiceWorkers-PWA/css/styles.css',
@@ -27,4 +29,12 @@ self.addEventListener('activate', e => {
 // Fetch
 self.addEventListener('fetch', e => {
   console.log('fetch...',e)
+
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => {
+        return response
+      })
+      .catch( () => caches.match('/47-ServiceWorkers-PWA/error.html'))
+  )
 })
