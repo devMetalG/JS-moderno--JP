09d02b6ec25a8db09ae5c249581c2a544489018e
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-10 15:39:32 -0600

    add: edit y delete citas Jest

--- 51-Testing-Jest/__tests__/citas.js
+++ 51-Testing-Jest/__tests__/citas.js
@@ -3,10 +3,12 @@ import Citas from '../js/classes/Citas'
 describe('Probar clase Citas',() => {
   
   const citas = new Citas()
+  const id = Date.now()
   
   test('agregar nueva cita',()=>{
     const citaObj = {
       mascota: 'Morris',
+      id,
       propietario: 'Memo',
       telefono: '1111112222',
       fecha: '11-10-2023',
@@ -17,6 +19,29 @@ describe('Probar clase Citas',() => {
     citaObj.id = Date.now()
     
     citas.agregarCita(citaObj)
+
+    expect(citas).toMatchSnapshot()
+  })
+
+  test('Actualizar Cita', () => {
+    const citaActualizada = {
+      mascota: 'Euphy',
+      id,
+      propietario: 'Costal',
+      telefono: '1111112222',
+      fecha: '11-10-2023',
+      hora: '15:10',
+      sintomas: 'sindrome del gato paracaidista'
+    }
+
+    citas.editarCita(citaActualizada)
+
+    expect(citas).toMatchSnapshot()
+  })
+  
+  test('Eliminar Cita', () => {
+
+    citas.eliminarCita(id)
 
     expect(citas).toMatchSnapshot()
   })
