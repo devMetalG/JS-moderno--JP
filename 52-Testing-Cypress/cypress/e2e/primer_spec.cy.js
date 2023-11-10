8ea883e1d2ab33400b8f044eaf835f9e300be378
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-10 17:56:06 -0600

    add: atributos en CYPRESS

--- 52-Testing-Cypress/cypress/e2e/primer_spec.cy.js
+++ 52-Testing-Cypress/cypress/e2e/primer_spec.cy.js
@@ -1,7 +1,20 @@
+/// <reference types="cypress"/>
+
 describe('Carga la pagina principal', () => {
   it('Carga pagina principal', () => {
     cy.visit('index.html')
-    cy.contains('h1', 'Administrador de Pacientes de Veterinaria')
-    cy.get('h1').should('exist')
+    // Verifica el elemento y su texto
+    cy.contains('[data-cy="project-title"]', 'Administrador de Pacientes de Veterinaria')
+    // Verifica que exista
+    cy.get('[data-cy="project-title"]').should('exist')
+    // Verifica que exista el elemento y contenga un texto especifico
+    cy.get('[data-cy="project-title"]')
+      .invoke('text')
+      .should('equal', 'Administrador de Pacientes de Veterinaria')
+
+    // Verificar el texto de las citas
+    cy.get('[data-cy=citas-heading]')
+      .invoke('text')
+      .should('equal', 'No hay Citas, comienza creando una')
   })
 })
