489efcef2734f2eae689530ce696af31d25b8bff
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-10 22:30:52 -0600

    add: eliminar cita CYPRESS

---
+++ 52-Testing-Cypress/cypress/e2e/eliminar_cita.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress"/>
+
+describe('Crea una cita y la elimina', () => {
+  it('llena los campos y crea una cita', () => {
+    cy.visit('index.html')
+
+    cy.get('[data-cy=mascota]')
+      .type('Euphy')
+
+    cy.get('[data-cy=propietario]')
+      .type('Costal')
+
+    cy.get('[data-cy=telefono]')
+      .type('4771234576')
+
+    cy.get('[data-cy=fecha]')
+      .type('2023-11-12')
+
+    cy.get('[data-cy=hora]')
+      .type('11:30')
+
+    cy.get('[data-cy=sintomas]')
+      .type('Sindrome del gato volador')
+
+    cy.get('[data-cy=submit]')
+      .click()
+      
+    cy.get('[data-cy=citas-heading]')
+      .invoke('text')
+      .should('equal', 'Administra tus Citas')
+
+    cy.get('[data-cy=alerta]')
+      .invoke('text')
+      .should('equal', 'Se agregó correctamente')
+    
+    cy.get('[data-cy=alerta]')
+      .should('have.class', 'alert-success')
+  })
+  it('Elimina una cita', () => {
+    cy.get('[data-cy="btn-eliminar"]')
+      .click()
+  })
+})
--- 52-Testing-Cypress/js/classes/UI.js
+++ 52-Testing-Cypress/js/classes/UI.js
@@ -82,7 +82,7 @@ class UI {
 
             btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
-            btnEliminar.dataset.cy = 'eliminar-btn'
+            btnEliminar.dataset.cy = 'btn-eliminar'
 
             // Añade un botón de editar...
             const btnEditar = document.createElement('button');
